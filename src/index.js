8175fafb420ec977ad86fe5f428aafeaa993afd6
Luys Fernnando <luysfernnando.13@gmail.com>
2023-01-08 03:51:58 -0300

    Adicionado Troca de Turnos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/*
 class Square extends React.Component {
   render() {
     return (
@@ -14,19 +15,33 @@ class Square extends React.Component {
     );
   }
 }
+*/
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
   }
 
   handleclick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
